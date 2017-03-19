ccc6a02690ee5f7bd38780ec0846456eefdb6681
syammohan <syam.mohan.mec@gmail.com>
2017-03-19 15:42:44 -0400

    Modified product comp

--- angular-course/apm/app/products/product-list.component.js
+++ angular-course/apm/app/products/product-list.component.js
@@ -34,7 +34,17 @@ var ProductListComponent = (function () {
                 "price": 32.99,
                 "starRating": 4.2,
                 "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-            }
+            },
+            {
+                "productId": 5,
+                "productName": "Hammer",
+                "productCode": "TBX-0048",
+                "releaseDate": "May 21, 2016",
+                "description": "Curved claw steel hammer",
+                "price": 8.9,
+                "starRating": 4.8,
+                "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+            },
         ];
     }
     ProductListComponent.prototype.toggleImage = function () {
--- angular-course/apm/app/products/product-list.component.ts
+++ angular-course/apm/app/products/product-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core"
-import {IProduct} from "./product"
+import { IProduct } from "./product"
 
 @Component({
     selector: 'pm-products',
@@ -12,7 +12,7 @@ export class ProductListComponent implements OnInit {
     imageMargin: number = 2;
     imageWidth: number = 50;
     showImage: boolean = false;
-    productListFilter: string ='cart';
+    productListFilter: string = 'cart';
     products: IProduct[] = [
         {
             "productId": 1,
@@ -33,12 +33,22 @@ export class ProductListComponent implements OnInit {
             "price": 32.99,
             "starRating": 4.2,
             "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        }
+        },
+        {
+            "productId": 5,
+            "productName": "Hammer",
+            "productCode": "TBX-0048",
+            "releaseDate": "May 21, 2016",
+            "description": "Curved claw steel hammer",
+            "price": 8.9,
+            "starRating": 4.8,
+            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+        },
     ];
     toggleImage(): void {
         this.showImage = !this.showImage;
     }
-    ngOnInit(): void{
+    ngOnInit(): void {
         console.log('In OnInit');
     }
 }
