e013e6184cb8b7423daaef491c4bd0cf1da78431
syammohan <syam.mohan.mec@gmail.com>
2017-03-23 23:43:32 -0400

    Added Routing

--- angular-course/apm/app/app.module.js
+++ angular-course/apm/app/app.module.js
@@ -14,6 +14,10 @@ var app_component_1 = require("./app.component");
 var product_list_component_1 = require("./products/product-list.component");
 var product_filter_pipe_1 = require("./products/product-filter.pipe");
 var star_component_1 = require("./shared/star.component");
+var welcome_component_1 = require("./home/welcome.component");
+var product_detail_component_1 = require("./products/product-detail.component");
+var router_1 = require("@angular/router");
+var product_guard_service_1 = require("./products/product-guard.service");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -21,8 +25,15 @@ var AppModule = (function () {
 }());
 AppModule = __decorate([
     core_1.NgModule({
-        imports: [platform_browser_1.BrowserModule, forms_1.FormsModule, http_1.HttpModule],
-        declarations: [app_component_1.AppComponent, product_list_component_1.ProductListComponent, product_filter_pipe_1.ProductFilterPipe, star_component_1.StarComponent],
+        imports: [platform_browser_1.BrowserModule, forms_1.FormsModule, http_1.HttpModule, router_1.RouterModule.forRoot([
+                { path: 'products', component: product_list_component_1.ProductListComponent },
+                { path: 'product/:id', canActivate: [product_guard_service_1.ProductDetailGuard], component: product_detail_component_1.ProductDetailComponent },
+                { path: 'welcome', component: welcome_component_1.WelcomeComponent },
+                { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+                { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+            ])],
+        declarations: [app_component_1.AppComponent, product_list_component_1.ProductListComponent, product_filter_pipe_1.ProductFilterPipe, star_component_1.StarComponent, welcome_component_1.WelcomeComponent, product_detail_component_1.ProductDetailComponent],
+        providers: [product_guard_service_1.ProductDetailGuard],
         bootstrap: [app_component_1.AppComponent]
     })
 ], AppModule);
--- angular-course/apm/app/app.module.ts
+++ angular-course/apm/app/app.module.ts
@@ -6,10 +6,21 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ProductFilterPipe } from './products/product-filter.pipe'
 import { StarComponent } from './shared/star.component'
+import { WelcomeComponent } from "./home/welcome.component";
+import { ProductDetailComponent } from "./products/product-detail.component";
+import { RouterModule } from '@angular/router'
+import { ProductDetailGuard } from "./products/product-guard.service";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpModule],
-  declarations: [AppComponent, ProductListComponent, ProductFilterPipe, StarComponent],
+  imports: [BrowserModule, FormsModule, HttpModule, RouterModule.forRoot([
+    { path: 'products', component: ProductListComponent },
+    { path: 'product/:id', canActivate: [ProductDetailGuard], component: ProductDetailComponent },
+    { path: 'welcome', component: WelcomeComponent },
+    { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+    { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+  ])],
+  declarations: [AppComponent, ProductListComponent, ProductFilterPipe, StarComponent, WelcomeComponent, ProductDetailComponent],
+  providers: [ProductDetailGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
