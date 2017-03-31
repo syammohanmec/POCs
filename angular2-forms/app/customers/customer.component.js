054f6975d6116cc7f119e0b61f731a1d5766b551
syammohan <syam.mohan.mec@gmail.com>
2017-03-31 16:19:57 -0400

    Added Custom Validations

--- angular2-forms/app/customers/customer.component.js
+++ angular2-forms/app/customers/customer.component.js
@@ -5,16 +5,73 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+//import { NgForm } from '@angular/forms';
+var forms_1 = require("@angular/forms");
 var customer_1 = require("./customer");
+//--- Custom Validator without parameters 
+// function ratingRange(control: AbstractControl): { [key: string]: boolean } | null {
+//     if (control.value != undefined && (isNaN(control.value) || control.value < 1 || control.value > 5))
+//         return { 'range': true };
+//     else
+//         return null;
+// }
+function ratingRange(min, max) {
+    return function (control) {
+        if (control.value != undefined && (isNaN(control.value) || control.value < min || control.value > max))
+            return { 'range': true };
+        else
+            return null;
+    };
+}
+function emailMatcher(control) {
+    var email = control.get('email');
+    var confirmEmail = control.get('confirmEmail');
+    if (email.pristine || confirmEmail.pristine) {
+        return null;
+    }
+    if (email.value === confirmEmail.value) {
+        return null;
+    }
+    return { match: true };
+}
 var CustomerComponent = (function () {
-    function CustomerComponent() {
+    function CustomerComponent(fb) {
+        this.fb = fb;
         this.customer = new customer_1.Customer();
     }
-    CustomerComponent.prototype.save = function (customerForm) {
-        console.log(customerForm.form);
-        console.log('Saved: ' + JSON.stringify(customerForm.value));
+    CustomerComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        this.customerForm = this.fb.group({
+            firstName: ['', [forms_1.Validators.required, forms_1.Validators.minLength(3)]],
+            lastName: ['', [forms_1.Validators.required, forms_1.Validators.maxLength(50)]],
+            emailGroup: this.fb.group({
+                email: ['', [forms_1.Validators.required, forms_1.Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+')]],
+                confirmEmail: ['', [forms_1.Validators.required]],
+            }, { validator: emailMatcher }),
+            phone: '',
+            notification: 'email',
+            rating: ['', ratingRange(1, 5)],
+            sendCatalog: true
+        });
+        this.customerForm.get('notification').valueChanges
+            .subscribe(function (value) { return _this.setNotification(value); });
+    };
+    CustomerComponent.prototype.save = function () {
+        console.log(this.customerForm);
+        console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+    };
+    CustomerComponent.prototype.setNotification = function (notifyVia) {
+        var phoneControl = this.customerForm.get('phone');
+        if (notifyVia === "text")
+            phoneControl.setValidators(forms_1.Validators.required);
+        else
+            phoneControl.clearValidators();
+        phoneControl.updateValueAndValidity();
     };
     return CustomerComponent;
 }());
@@ -22,7 +79,8 @@ CustomerComponent = __decorate([
     core_1.Component({
         selector: 'my-signup',
         templateUrl: './app/customers/customer.component.html'
-    })
+    }),
+    __metadata("design:paramtypes", [forms_1.FormBuilder])
 ], CustomerComponent);
 exports.CustomerComponent = CustomerComponent;
 //# sourceMappingURL=customer.component.js.map
--- angular2-forms/app/customers/customer.component.ts
+++ angular2-forms/app/customers/customer.component.ts
@@ -1,17 +1,88 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-
+import { Component, OnInit } from '@angular/core';
+//import { NgForm } from '@angular/forms';
+import { FormBuilder, FormGroup, Validator, Validators, AbstractControl, ValidatorFn } from '@angular/forms'
 import { Customer } from './customer';
+
+//--- Custom Validator without parameters 
+// function ratingRange(control: AbstractControl): { [key: string]: boolean } | null {
+//     if (control.value != undefined && (isNaN(control.value) || control.value < 1 || control.value > 5))
+//         return { 'range': true };
+//     else
+//         return null;
+// }
+
+function ratingRange(min: number, max: number): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: boolean } | null => {
+        if (control.value != undefined && (isNaN(control.value) || control.value < min || control.value > max))
+            return { 'range': true };
+        else
+            return null;
+    }
+}
+
+function emailMatcher(control: AbstractControl): { [key: string]: boolean } | null {
+    let email = control.get('email');
+    let confirmEmail = control.get('confirmEmail');
+
+    if (email.pristine || confirmEmail.pristine) {
+        return null;
+    }
+    if (email.value === confirmEmail.value) {
+        return null;
+    }
+    return { match: true };
+}
 
 @Component({
     selector: 'my-signup',
     templateUrl: './app/customers/customer.component.html'
 })
-export class CustomerComponent  {
-    customer: Customer= new Customer();
+export class CustomerComponent implements OnInit {
 
-    save(customerForm: NgForm) {
-        console.log(customerForm.form);
-        console.log('Saved: ' + JSON.stringify(customerForm.value));
+    customer: Customer = new Customer();
+    customerForm: FormGroup;
+
+    constructor(private fb: FormBuilder) {
+
     }
- }
+
+
+    ngOnInit(): void {
+        this.customerForm = this.fb.group({
+            firstName: ['', [Validators.required, Validators.minLength(3)]],
+            lastName: ['', [Validators.required, Validators.maxLength(50)]],
+            emailGroup: this.fb.group({
+                email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+')]],
+                confirmEmail: ['', [Validators.required]],
+            }, { validator: emailMatcher }),
+            phone: '',
+            notification: 'email',
+            rating: ['', ratingRange(1, 5)],
+            sendCatalog: true
+        });
+
+        this.customerForm.get('notification').valueChanges
+                        .subscribe(value => this.setNotification(value));
+
+    }
+
+    save(): void {
+        console.log(this.customerForm);
+        console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+    }
+    setNotification(notifyVia: string): void {
+        let phoneControl = this.customerForm.get('phone');
+        if (notifyVia === "text")
+            phoneControl.setValidators(Validators.required);
+        else
+            phoneControl.clearValidators();
+        phoneControl.updateValueAndValidity();
+    }
+
+    // --Template Driven Code--
+
+    // save(customerForm: NgForm) {
+    //     console.log(customerForm.form);
+    //     console.log('Saved: ' + JSON.stringify(customerForm.value));
+    // }
+}
